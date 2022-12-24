5b4665a9c322e16184ef3ee22dd6f16a22f80369
chitwang <101563468+chitwang@users.noreply.github.com>
2022-12-24 21:13:54 +0530

    Add files via upload

---
+++ src/Components/carousel.js
@@ -0,0 +1,85 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function CarouselFadeExample() {
+  return (
+    // <Carousel fade>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100 h-100"
+    //       src="images/2.png"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>First slide label</h3>
+    //       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="images/red.jpg"
+    //       alt="Second slide"
+    //     />
+
+    //     <Carousel.Caption>
+    //       <h3>Second slide label</h3>
+    //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="images/2.png"
+    //       alt="Third slide"
+    //     />
+
+    //     <Carousel.Caption>
+    //       <h3>Third slide label</h3>
+    //       <p>
+    //         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+    //       </p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    // </Carousel>
+    <>
+      <div class="row">
+  <div class="column">
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="falls2.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="nature.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+  </div>
+  <div class="column">
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="mountainskies.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+  </div>
+  <div class="column">
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="falls2.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="nature.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+  </div>
+  <div class="column">
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+    <img src="mountainskies.jpg"></img>
+    <img src="images/2.png"></img>
+    <img src="images/2.png"></img>
+  </div>
+</div>
+    </>
+  );
+}
+
+export default CarouselFadeExample;
